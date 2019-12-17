211cfbc7cba4ab5c272816184be21d801363d0a7
narekaspaturyan <58630709+narekaspaturyan@users.noreply.github.com>
2019-12-18 01:03:12 +0400

    Add files via upload

---
+++ homework-3 ex 1.js
@@ -0,0 +1,9 @@
+
+
+// // Homework 3
+
+// exercise 1
+
+for (var i=1; i<=10; i++ ){
+    console.log(i)
+};
---
+++ homework-3 ex 8.js
@@ -0,0 +1,47 @@
+
+
+// // Homework 3
+
+// // exercise 8
+
+for(var i = 1; i<=12; i++){
+    if(i===12 && 1 && 2)
+    console.log('winter');
+    else if (i === 3 && 4 && 5)
+    console.log('spring');
+    else if (i === 6 && 7 && 8)
+    console.log('summer');
+   else if (i === 9 && 10 && 11)
+    console.log('automn')
+}
+         
+//////////////////////////
+
+for(var i = 1; i<=12; i++){
+    if(i===1)
+    console.log('winter');
+    else if (i === 2 )
+    console.log('winter');
+    else if (i === 3 )
+    console.log('spring');
+   else if (i === 4)
+    console.log('spring')
+    else if(i=== 5 )
+    console.log('spring');
+    else if (i ===6 )
+    console.log('summer');
+    else if (i === 7 )
+    console.log('summer');
+   else if (i === 8)
+   console.log('summer')
+   else if(i=== 9 )
+    console.log('automn');
+    else if (i === 10)
+    console.log('automn');
+    else if (i === 11)
+    console.log('automn');
+   else if (i === 12)
+    console.log('winter')
+         
+}
+    
