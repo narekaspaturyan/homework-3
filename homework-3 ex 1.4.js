57deab2c40cffaef80ce8957e80a935cfbbf2589
narekaspaturyan <58630709+narekaspaturyan@users.noreply.github.com>
2019-12-20 00:36:45 +0400

    Add files via upload

---
+++ homework-3 ex 1.4.js
@@ -0,0 +1,27 @@
+var numberGrid = [
+    [ "*         * "],
+    [ " *       * "],
+    [ "  *     * "],
+    [ "   *   * "],
+    [  "     *     "],
+    [ "   *   *"],
+    [ "  *     *"],
+     ["*         *"],
+    ["*           *"],
+   
+   ]
+   for (var i = 0; i<numberGrid.length; i++){
+   
+   
+     for(var j= 0; j<numberGrid[i].length; j++) {//////////important
+   
+      console.log(numberGrid[i][j]);
+     }
+   };
+
+
+
+
+
+
+
